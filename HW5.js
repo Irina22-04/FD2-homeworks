5223d01c165c6222a73fccf3df7ef8cb2fb97fad
Irina22-04 <baranovskaya.irina.87@mail.ru>
2020-01-28 20:13:24 +0300

    solved homework 5

--- HW5.js
+++ HW5.js
@@ -1,7 +1,7 @@
 function treeSum(arr) {
     let result = 0;
     for (let i = 0; i < arr.length; i++) {
-        if (typeof (arr[i]) === 'number') {
+        if (typeof arr[i] === 'number') {
             result += arr[i];
         } else {
             result += treeSum(arr[i]);
