dc9e0b11ec78045212a2a09be5611027094d6f09
Irina22-04 <baranovskaya.irina.87@mail.ru>
2020-02-04 19:24:21 +0300

    solved homework 7

---
+++ HW8.js
@@ -0,0 +1,22 @@
+function myBind(func, context, ...arg){
+    return function() {
+        return func.apply(context, arg);
+    }
+}
+
+const student = {
+    name: 'Саша',
+    age: 25,
+}
+
+function favoriteSubj(arg1, arg2) {
+    console.log(`${this.name}: любимые предметы ${arg1} и ${arg2}`);
+}
+
+myBind(favoriteSubj, student, 'математика', 'физика')();
+
+function sum(a, b, c) {
+    return a + b + c;
+}
+
+console.log(myBind(sum, null, 3, 4, 5)());
