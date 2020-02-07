22ca65b123a3f012990077a59e2ac5bbc0860401
Irina22-04 <baranovskaya.irina.87@mail.ru>
2020-02-07 20:20:25 +0300

    solved homework 9

--- HW9.js
+++ HW9.js
@@ -16,3 +16,14 @@ function HashStorageFunc() {
         return Object.keys(this.hash);
     };
 }
+
+const drinkStorage = new HashStorageFunc();
+drinkStorage.addValue('морс', {'состав:': 'вода, варенье', 'алкоголь': false});
+console.log(drinkStorage.getKeys());
+console.log(drinkStorage.getValue('морс'));
+console.log(drinkStorage.deleteValue('компот'));
+console.log(drinkStorage.deleteValue('морс'));
+console.log(drinkStorage.getKeys());
+drinkStorage.addValue('компот', {'состав:': 'вода, фрукты', 'алкоголь': false});
+drinkStorage.addValue('каппучино', {'состав:': 'вода, кофе, сахар, молоко', 'алкоголь': false});
+console.log(drinkStorage.getKeys());
