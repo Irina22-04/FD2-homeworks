9c8e50987eea7a95dffa8ea955123c2c0b1406d1
Irina22-04 <baranovskaya.irina.87@mail.ru>
2020-02-15 20:12:55 +0300

    solved homework 12

--- HW12.js
+++ HW12.js
@@ -3,5 +3,4 @@ const inRange = (min, max) => {
         return value >= min && value <= max;
     }
 }
-const arr = [0, 1, 1.5, 17, 9, 0.5];
-console.log(arr.filter(inRange(1, 10)));
+arr.filter(inRange(min, max));
